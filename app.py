c936b40abac624bd5d442c11ce835c68c6a5b918
Vasu Bhimani <112377821+VasuBhimani@users.noreply.github.com>
2024-10-12 00:40:31 +0530

    Update app.py
    port change

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def delete_image(image_name):
         return jsonify({"message": "Failed to delete image", "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8123)
+
